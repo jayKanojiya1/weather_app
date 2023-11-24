18c9943a8dd4e5a4a92d4a8f02c9b00f0373ffc3
jay_kanojiya_10 <jaykanojiya1010@gmail.com>
2023-11-24 17:17:34 +0530

    first commit

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -10,14 +10,14 @@
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="<?php echo URL;?>/assets/css/style.css">
-	<title>Hello, world!</title>
+	<title>Weather Details</title>
   </head>
   <body>
   <div class="container-fluid px-1 px-sm-3 py-5 mx-auto">
     <div class="row d-flex justify-content-center">
         <div class="row card0">
             <div class="card1 col-lg-8 col-md-7">
-                <small>the.weather</small>
+                <small>Weather Report</small>
                 <div class="text-center">
                     <img class="image mt-5" src="https://i.imgur.com/M8VyA2h.png">
                 </div>
@@ -25,12 +25,9 @@
                     <h1 class="large-font mr-3" id ="temp">26&#176;</h1>
                     <div class="d-flex flex-column mr-3">
                         <h2 class="mt-3 mb-0" id="city">London</h2>
-                        <small>10:36 - Tuesday, 22 Oct '19</small>
+                        <p id="date">10:36 - Tuesday, 22 Oct '19</p>
                     </div>
-                    <div class="d-flex flex-column text-center">
-                        <h3 class="fa fa-sun-o mt-4"></h3>
-                        <small>Sunny</small>
-                    </div>
+                   
                 </div>
             </div>
             <div class="card2 col-lg-4 col-md-5">
@@ -41,21 +38,18 @@
                 <div class="mr-5">
                    <p>Weather Details</p>
                     <div class="row px-3">
-                        <p class="light-text">Cloudy</p>
-                        <p class="ml-auto">12%</p>
+                        <p class="light-text">Weather</p>
+                        <p class="ml-auto" id="weather">12%</p>
                     </div>
                     <div class="row px-3">
                         <p class="light-text">Humidity</p>
-                        <p class="ml-auto">78%</p>
+                        <p class="ml-auto" id="humidity">78%</p>
                     </div>
                     <div class="row px-3">
                         <p class="light-text">Wind</p>
-                        <p class="ml-auto">1km/h</p>
+                        <p class="ml-auto" id="wind">1<span>km/h</span></p>
                     </div>
-                    <div class="row px-3">
-                        <p class="light-text">Rain</p>
-                        <p class="ml-auto">0mm</p>
-                    </div>
+                 
 
                     <div class="line mt-3"></div>
                 </div>
@@ -64,16 +58,9 @@
     </div>
 </div>
     <!-- Optional JavaScript -->
-
-
-
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <!-- <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script> -->
- 
+   
 <script>
 	function search(){
 		let location = $("#location").val();
@@ -96,20 +83,25 @@
 					return false;
 				}else{
 					console.log(object);
+				
+					const options = { day: 'numeric', month: 'short', year: 'numeric' };
+					let date = new Date(object.list[0].dt * 1000).toLocaleDateString('en-US', options);
 					let temp = Math.round(object.list[0].main.temp - 273.15);
+					let wind = Math.round(object.list[0].wind.speed);
 					let city = object.city.name;
+					let humidity = object.list[0].main.humidity;
+					let weather = object.list[0].weather[0].main;
+					
 					$("#city").text(city);
 					$("#temp").text(temp);
-
-					//  console.log(object.city.name);
-				}
-				// console.log(object.cod);
-
-				// if(response.cod == 404){
-				// 	alert(response.message);
+					$("#date").text(date);
+					$("#humidity").text(humidity+"%");
 					
-				// }
-				// console.log(response);
+					$("#wind").text(wind+"km/h");
+					$("#weather").text(weather);
+
+				}
+			
 
 			},
 			error: function(error){
